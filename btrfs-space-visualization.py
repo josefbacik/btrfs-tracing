b4d8558be0d9a2dba98510d13d0ac7c24993270f
Josef Bacik <jbacik@fb.com>
2016-01-11 14:07:52 -0500

    make btrfs-space-visualization actually work
    Some bugfixes, added some leak detection for fun, deal with instances properly,
    and suppress the trace-cmd related output.

--- btrfs-space-visualization.py
+++ btrfs-space-visualization.py
@@ -5,10 +5,9 @@ from tracecmd import Trace
 from ctracecmd import pevent_register_comm
 from ctracecmd import pevent_data_comm_from_pid
 from ctracecmd import py_supress_trace_output
+from ctracecmd import tracecmd_buffer_instances
+from ctracecmd import tracecmd_buffer_instance_handle
 
-BTRFS_BLOCK_GROUP_DATA = (1 << 0)
-BTRFS_BLOCK_GROUP_METADATA = (1 << 1)
-BTRFS_BLOCK_GROUP_SYSTEM = (1 << 2)
 
 class Blockgroup:
     def __init__(self, offset, size):
@@ -16,25 +15,28 @@ class Blockgroup:
         self.size = size;
 
 class Spaceinfo:
+    BTRFS_BLOCK_GROUP_DATA = (1 << 0)
+    BTRFS_BLOCK_GROUP_METADATA = (1 << 1)
+    BTRFS_BLOCK_GROUP_SYSTEM = (1 << 2)
+
     def __init__(self, flags):
-        self.flags = flags & (BTRFS_BLOCK_GROUP_DATA |
-                              BTRFS_BLOCK_GROUP_METADATA |
-                              BTRFS_BLOCK_GROUP_SYSTEM)
+        self.flags = flags & (self.BTRFS_BLOCK_GROUP_DATA |
+                              self.BTRFS_BLOCK_GROUP_METADATA |
+                              self.BTRFS_BLOCK_GROUP_SYSTEM)
         self.size = 0
         self.bytes_used = 0
         self.bytes_readonly = 0
         self.bytes_may_use = 0
         self.reservations = {}
 
-    def add_block_group(self, bytes, bytes_used, bytes_super):
-        self.size += bytes
+    def add_block_group(self, size, bytes_used, bytes_super):
+        self.size += size
         self.bytes_used += bytes_used
         self.bytes_readonly += bytes_super
 
 def find_block_group(block_groups, offset):
     for  block_group in block_groups:
-        if (block_group.offset <= offset) and
-           ((block_group.offset + block_group.size) > offset)
+        if (block_group.offset <= offset) and ((block_group.offset + block_group.size) > offset):
             return block_group
     return None
 
@@ -42,31 +44,41 @@ def find_space_info(space_infos, flags):
     for space_info in space_infos:
         if space_info.flags == flags:
             return space_info
-    return Spaceinfo(flags)
+    space_info = Spaceinfo(flags)
+    space_infos.append(space_info)
+    return space_info
 
 def parse_tracefile(infile):
     trace = Trace(infile)
 
+    instances = tracecmd_buffer_instances(trace._handle)
+    if instances != 0:
+        new_handle = tracecmd_buffer_instance_handle(trace._handle, 0)
+        trace._handle = new_handle
+
     reservations = {}
-    block_groups = {}
-    space_infos = {}
+    block_groups = []
+    space_infos = []
     while True:
         rec = trace.read_next_event()
-        if not rec
+        if not rec:
             break
         if rec.name == "btrfs_add_block_group":
+            print("adding block group %d-%d" % (rec.num_field("offset"),
+            rec.num_field("size")))
             space_info = find_space_info(space_infos, rec.num_field("flags"))
-            space_info.add_block_group(rec.num_field("bytes"),
+            space_info.add_block_group(rec.num_field("size"),
                                        rec.num_field("bytes_used"),
                                        rec.num_field("bytes_super"))
             block_group = Blockgroup(rec.num_field("offset"),
-                                     rec.num_field("bytes"))
+                                     rec.num_field("size"))
+            block_groups.append(block_group)
             block_group.space_info = space_info
         if rec.name == "btrfs_space_reservation":
             reserve_type = rec.str_field("type")
             if "enospc" in reserve_type:
                 space_info = find_space_info(space_infos,
-                                             rec.num_field("flags"))
+                                             rec.num_field("val"))
                 print("Hit enospc, dumping info\n")
                 for r,val in reservations:
                     print("%s: %d\n" % (r, val))
@@ -75,7 +87,7 @@ def parse_tracefile(infile):
                          space_info.bytes_used, space_info.bytes_readonly))
             elif "space_info" in reserve_type:
                 space_info = find_space_info(space_infos,
-                                             rec.num_field("flags"))
+                                             rec.num_field("val"))
                 if rec.num_field("reserve") == 1:
                     space_info.bytes_may_use += rec.num_field("bytes")
                 else:
@@ -87,8 +99,7 @@ def parse_tracefile(infile):
             else:
                 reservations[reserve_type] -= rec.num_field("bytes")
 
-        if rec.name == "btrfs_reserved_extent_alloc" or
-            rec.name == "btrfs_reserved_extent_free":
+        if rec.name == "btrfs_reserved_extent_alloc" or rec.name == "btrfs_reserved_extent_free":
             block_group = find_block_group(block_groups,
                                            rec.num_field("start"))
             if not block_group:
@@ -100,7 +111,19 @@ def parse_tracefile(infile):
                 space_info.bytes_used += rec.num_field("len")
             else:
                 space_info.bytes_used -= rec.num_field("len")
-
+    num_leaks = 0
+    for space_info in space_infos:
+        if space_info.bytes_may_use != 0:
+            print("Bytes may use leak for space info %d, bytes_may_use %d\n"  %
+                  (space_info.flags, space_info.bytes_may_use))
+            num_leaks += 1
+        for name,value in space_info.reservations:
+            if value != 0:
+                print("Reservation for %s outstanding in space info %d, " +
+                      "value %d" % (name, space_info.flags, value))
+                num_leaks += 1
+    if num_leaks == 0:
+        print("Yay no leaks!")
 def record_events():
     events = [ "btrfs:btrfs_add_block_group",
                "btrfs:btrfs_space_reservation",
@@ -110,7 +133,7 @@ def record_events():
 
     cmd = [ 'trace-cmd', 'record', '-B', 'enospc', ]
     for e in events:
-        cmd.append(['-e', e])
+        cmd.extend(['-e', e])
     subprocess.call(cmd)
 
 if __name__ == "__main__":
@@ -123,9 +146,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.record:
-        return record_events():
-
-    infile = "trace.dat"
-    if args.infile:
-        infile = args.infile
-    parse_tracefile(infile)
+        record_events()
+    else:
+        py_supress_trace_output()
+        infile = "trace.dat"
+        if args.infile:
+            infile = args.infile
+        parse_tracefile(infile)
